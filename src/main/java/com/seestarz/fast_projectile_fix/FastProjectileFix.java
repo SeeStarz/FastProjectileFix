c73714a20957b8ec358a38cafcd71d2a80062fb6
SeeStarz <69336968+SeeStarz@users.noreply.github.com>
2022-11-07 23:34:59 +0700

    Update FastProjectileFix.java

--- src/main/java/com/seestarz/fast_projectile_fix/FastProjectileFix.java
+++ src/main/java/com/seestarz/fast_projectile_fix/FastProjectileFix.java
@@ -1,10 +1,7 @@
 package com.seestarz.fast_projectile_fix;
 
-import com.seestarz.fast_projectile_fix.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,9 +14,6 @@ public class FastProjectileFix
     public static final Logger LOGGER = LogManager.getLogger();
 
     public FastProjectileFix() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ModItems.register(eventBus);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
